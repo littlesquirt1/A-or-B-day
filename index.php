2981b0cc41a5799decaf396767efaee222ee5920
littlesquirt1 <56567956+littlesquirt1@users.noreply.github.com>
2022-02-15 18:10:20 +0000

    fixex \n

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 $command = escapeshellcmd('python main.py');
 $output = shell_exec($command);
 
-echo '<!DOCTYPE html>\n';
-echo '<html lang="en">\n';
-echo '<head>\n';
-echo '<meta charset="UTF-8">\n';
-echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">\n';
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">\n';
-echo '<title>A or B Day?</title>\n';
+echo '<!DOCTYPE html>' . "\n";
+echo '<html lang="en">' . "\n";
+echo '<head>' . "\n";
+echo '<meta charset="UTF-8">' . "\n";
+echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">' . "\n";
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">' . "\n";
+echo '<title>A or B Day?</title>' . "\n";
 echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
 echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
 echo '<link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">';
-echo '</head>\n';
-echo '<body>\n';
-echo '<p style="font-family: &quot;Luckiest Guy&quot;, cursive;">\n';
+echo '</head>' . "\n";
+echo '<body>' . "\n";
+echo '<p style="font-family: &quot;Luckiest Guy&quot;, cursive;">' . "\n";
 echo $output . "\n";
-echo '</p>';
-echo '</body>\n';
-echo '</html>\n';
+echo '</p>' . "\n";
+echo '</body>' . "\n";
+echo '</html>';
 ?>

d7ebfaa8783f5f72f87bd5de22e6db26a00677f0
littlesquirt1 <56567956+littlesquirt1@users.noreply.github.com>
2022-02-17 15:03:30 +0000

    made my code not dumb

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from ics import Calendar
 import requests
 from datetime import datetime
+import arrow
 
 url = "https://bedfordfms.sharpschool.net/ICalendarHandler?calendarId=1079243"
 c = Calendar(requests.get(url).text)
 today  = datetime.today().strftime('%Y-%m-%d')
+eventsnow = []
 
 for event in c.events:
   if str(event.begin.date()) == today:
-    if event.name == "A Day":
-      message = "It is an A Day"
-    elif event.name == "B Day":
-      message = "It is a B Day"
-    else:
-      message = "There is no school today"
+    eventsnow.append(event)
 
-print (message)
-
+if "A Day" in str(eventsnow):
+  print ("It is an A Day")
+elif "B Day" in str(eventsnow):
+ print ("It is a B Day")
+else:
+  print ("There is no school today")
